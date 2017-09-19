cae9de0a21c57380ba4946f5181e68bf875719f6
jacobcarey93 <31251057+jacobcarey93@users.noreply.github.com>
2017-09-19 09:43:24 -0600

    completed miniproject

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+
 import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom"
 
 import store from './store';
 
 ReactDOM.render(
+<HashRouter>
   <Provider store={ store }>
     <App />
-  </Provider>, 
+  </Provider>
+</HashRouter>,
   document.getElementById('root')
 );
 registerServiceWorker();
